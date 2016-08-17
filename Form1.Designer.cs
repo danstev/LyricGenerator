da5db20e96e236fcd190e619857a01360445d325
Dan Ward <dan.steve@hotmail.co.uk>
2016-08-17 21:55:25 +0100

    Create and basic form stuff
    No major features implemented, most plan in a seperate notepad. Just
    setting basics up.

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LyricGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(25, 50);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(640, 480);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 555);
+            this.Controls.Add(this.Input);
+            this.Name = "MainForm";
+            this.Text = "Lyric Generator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Input;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LyricGenerator
+{
+    public partial class MainForm : Form
+    {
+
+        public FreqTable freqTable;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        
+
+        void readIntoTable()
+        {
+            char[] delim = { ' ', '.', ',', ':' };
+
+            string toParse = Input.Text;
+            string[] parsed = toParse.Split(delim);
+
+            foreach(string s in parsed)
+            {
+                freqTable.addToTable(s);   
+            }
+
+        }
+    }
+
+    public class FreqTable
+    {
+        List<Word> list = new List<Word>();
+
+        FreqTable()
+        {}
+
+        public void addToTable(string s)
+        {
+            list.Add(s);
+        }
+    }
+
+    public class Word
+    {
+        public string word;
+        public int freq;
+
+
+    }
+}
